8f0fd300a05af8e560e7fb82a83a2e33a3cdf46b
Marc31697 <mbrown.bmhs1@hotmail.com>
2022-02-20 14:27:53 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/marc31697/go-lambda-terraform
+
+go 1.17
+
+require github.com/aws/aws-lambda-go v1.28.0
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+)
---
+++ src/go-lambda-terraform.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyInput struct {
+	Action string `json:"action"`
+	Data string `json:"data"`
+}
+
+type MyResponse struct {
+	StatusCode string `json:"statusCode"`
+	StatusMessage string `json:"statusMessage"`
+}
+
+func handle(event MyInput) (MyResponse, error){
+	fmt.Printf("%v\n", event)
+
+	return MyResponse{StatusCode: "200", StatusMessage: "Success"}, nil
+}
+
+func main(){
+	lambda.Start(handle)
+}
